9a6bc24931a0e5fd0da51f804591b409cf937197
Artur Begyan <arturbegyan98@gmail.com>
2023-04-11 00:08:45 +0100

    feat: generate passive swap events skeleton

---
+++ src/lp-pnl/processPassiveSwapEvents/generatePassiveSwapEvents.ts
@@ -0,0 +1,65 @@
+
+
+import { SwapEventInfo } from '../../common/swaps/parseSwapEvent';
+import { BigQueryPositionRow } from '../../big-query-support';
+import { getOnChainFixedAndVariableTokenBalances } from './getOnChainFixedAndVariableTokenBalances';
+import { generatePassiveSwap } from './generatePassiveSwap';
+
+export type GeneratePassiveSwapEventsArgs = {
+
+    existingLpPositionRows: BigQueryPositionRow[],
+    currentTimestamp: number
+
+}
+
+export type GeneratePassiveSwapEventsReturn = {
+    passiveSwapEvents: SwapEventInfo[], 
+    affectedLps: BigQueryPositionRow[]
+}
+
+export const generatePassiveSwapEvents = async ({existingLpPositionRows, currentTimestamp}: GeneratePassiveSwapEventsArgs): GeneratePassiveSwapEventsReturn => {
+
+    let passiveSwapEvents: SwapEventInfo[] = [];
+    let affectedLps: BigQueryPositionRow[] = [];
+
+    for (let i=0; i < existingLpPositionRows.length; i++) { 
+
+        const positionRow: BigQueryPositionRow  = existingLpPositionRows[i];
+        const lastUpdatedTimestampLP: number = positionRow.lastUpdatedTimestamp;
+        const isInFuture: boolean = lastUpdatedTimestampLP > currentTimestamp;
+
+        if (!isInFuture) { 
+
+            const cachedVariableTokenBalance: number = positionRow.variableTokenBalance;
+            const cachedFixedTokenBalance: number = positionRow.fixedTokenBalance;
+            const vammAddress: string = positionRow.vammAddress;
+            const ownerAddress: string = positionRow.ownerAddress;
+            const tickLower: number = positionRow.tickLower; 
+            const tickUpper: number = positionRow.tickUpper;
+
+            // todo: get back once implementation is ready
+            const {onChainVariableTokenBalance, onChainFixedTokenBalance} = await getOnChainFixedAndVariableTokenBalances();
+
+            const cachedAndOnChainVariableTokenBalanceMatch = cachedVariableTokenBalance === onChainVariableTokenBalance;    
+            const cachedAndOnChainFixedTokenBalanceMatch = cachedFixedTokenBalance === onChainFixedTokenBalance;
+
+            if (cachedAndOnChainVariableTokenBalanceMatch && cachedAndOnChainFixedTokenBalanceMatch) {
+                console.log(`Variable and Fixed Token Balances match, no need for passive swap`); 
+            } else {
+                // todo: get back once implementation is done 
+                const passiveSwap: SwapEventInfo = await generatePassiveSwap();
+                passiveSwapEvents.push(passiveSwap);
+                affectedLps.push(positionRow); 
+            }
+
+        } else {
+            console.log(`this lp position was initialized in the future relative to event`);
+        }
+
+
+    }
+    
+    
+    return {passiveSwapEvents, affectedLps};
+
+}
