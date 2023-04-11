300d457dd9c4b4db32a89b6606903768ebd272fa
Artur Begyan <arturbegyan98@gmail.com>
2023-04-11 03:30:21 +0100

    feat: sync mints

--- src/lp-pnl/syncMints.ts
+++ src/lp-pnl/syncMints.ts
@@ -1,14 +1,42 @@
 
 import { BigQuery } from '@google-cloud/bigquery';
 import { AMM } from '@voltz-protocol/v1-sdk';
-import { getPreviousMintEvents } from '../common/swaps/getPreviousSwapEvents';
+import { getPreviousMintEvents } from '../common/mints/getPreviousMintEvents';
 
 
-export const syncPassiveSwaps = async (
+export const syncMints = async (
     bigQuery: BigQuery,
     amms: AMM[],
     previousBlockNumber: number,
   ): Promise<number> => {
 
+    const previousMintEvents = await getPreviousMintEvents(amms, previousBlockNumber);
+
+    let counter = 0;
+
+    const promises = Object.values(previousMintEvents).map(async ({ amm, mintEvents }) => {
+        const sortedSwapEvents = mintEvents.sort((a, b) => {
+          if (a.blockNumber === b.blockNumber) {
+            return a.transactionIndex - b.transactionIndex;
+          }
+    
+          return a.blockNumber - b.blockNumber;
+        });
+    
+        for (const swapEvent of sortedSwapEvents) {
+          await processMintEvent(bigQuery, amm, swapEvent);
+          counter++;
+        }
+      });
+
+    
+      const output = await Promise.allSettled(promises);
+    output.forEach((v) => {
+        if (v.status === 'rejected') {
+        throw v.reason;
+        }
+    });
+
+  return counter;
 
 }
