0c851a0c1205bd7b51a1bc76372c4c2f3e3d793a
Artur Begyan <arturbegyan98@gmail.com>
2023-04-11 03:58:59 +0100

    feat: note todo

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { Storage } from '@google-cloud/storage';
 import { PROJECT_ID } from './common';
 import { run } from './trader-pnl/run';
 
+
+// todo: will need to adjust this script to enable execution for both lps and traders
 async function authenticateImplicitWithAdc() {
   const storage = new Storage({
     projectId: PROJECT_ID,
