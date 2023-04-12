2afc0b7901c2ef19de960c27ed243f63eb049538
Artur Begyan <arturbegyan98@gmail.com>
2023-04-12 20:11:18 +0100

    feat: parsing tests

--- tests/mints-and-burns/processMintAndBurnEvent/parseMintOrBurnEvent.test.ts
+++ tests/mints-and-burns/processMintAndBurnEvent/parseMintOrBurnEvent.test.ts
@@ -1,11 +1,10 @@
-import { Event } from 'ethers';
+import { BigNumber, Event } from 'ethers';
 import { parseMintOrBurnEvent } from '../../../src/common/mints-and-burns/parseMintOrBurnEvent';
 import { mockedAMM } from '../../utils';
 
 describe('parse mint or burn event', () => {
-  it('parse mint or burn event', () => {
-    
-    
+  it('parse mint event', () => {
+
     const event = {
       blockHash: 'blockHash',
       transactionHash: 'transactionHash',
@@ -14,7 +13,7 @@ describe('parse mint or burn event', () => {
         owner: '0x0000',
         tickLower: -1200,
         tickUpper: 1200,
-        amount: 100
+        amount: BigNumber.from(10).pow(18).mul(10)
       },
     } as unknown as Event;
 
@@ -27,10 +26,47 @@ describe('parse mint or burn event', () => {
       ownerAddress: '0x0000',
       tickLower: -1200,
       tickUpper: 1200,
-      notionalDelta: 100,
+      notionalDelta: 1.201,
       rateOracle: 'rate-oracle',
       underlyingToken: 'token',
       marginEngineAddress: 'margin-engine',
     });
+
+
+
+
+
+  });
+
+  it('parse burn event', () => {
+
+    const event = {
+      blockHash: 'blockHash',
+      transactionHash: 'transactionHash',
+      logIndex: 1,
+      args: {
+        owner: '0x0000',
+        tickLower: -1200,
+        tickUpper: 1200,
+        amount: BigNumber.from(10).pow(18).mul(10)
+      },
+    } as unknown as Event;
+
+    const eventInfo = parseMintOrBurnEvent(1, mockedAMM, event, true); 
+
+    expect(eventInfo).toEqual({
+      eventId: 'blockhash_transactionhash_1',
+      chainId: 1,
+      vammAddress: 'amm-test',
+      ownerAddress: '0x0000',
+      tickLower: -1200,
+      tickUpper: 1200,
+      notionalDelta: -1.201,
+      rateOracle: 'rate-oracle',
+      underlyingToken: 'token',
+      marginEngineAddress: 'margin-engine',
+    });
+
+
   });
 });
