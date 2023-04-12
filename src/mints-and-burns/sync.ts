07cc6eb866543e956539417ec22d0780a6c0b2df
Artur Begyan <arturbegyan98@gmail.com>
2023-04-12 18:04:52 +0100

    feat: sync mint or burn

---
+++ src/mints-and-burns/sync.ts
@@ -0,0 +1,28 @@
+import { BigQuery } from '@google-cloud/bigquery';
+import { AMM } from '@voltz-protocol/v1-sdk';
+
+import { getPreviousEvents } from '../common';
+import { processMintOrBurnEvent } from './processMintAndBurnEvent';
+
+export const sync = async (
+  chainId: number,
+  bigQuery: BigQuery,
+  amms: AMM[],
+  fromBlock: number,
+  toBlock: number,
+): Promise<void> => {
+  const previousMintEvents = await getPreviousEvents(amms, ['mint', 'burn'], fromBlock, toBlock);
+
+  const promises = Object.values(previousMintEvents).map(async ({ amm, events }) => {
+    for (const event of events) {
+      await processMintOrBurnEvent(chainId, bigQuery, amm, event);
+    }
+  });
+
+  const output = await Promise.allSettled(promises);
+  output.forEach((v) => {
+    if (v.status === 'rejected') {
+      throw v.reason;
+    }
+  });
+};
