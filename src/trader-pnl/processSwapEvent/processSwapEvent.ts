94e0e86983ad8b07a12d6615beff242bcc168828
Artur Begyan <arturbegyan98@gmail.com>
2023-04-13 15:23:17 +0100

    fix: bug fix

--- src/trader-pnl/processSwapEvent/processSwapEvent.ts
+++ src/trader-pnl/processSwapEvent/processSwapEvent.ts
@@ -11,7 +11,7 @@ export const processSwapEvent = async (
   bigQuery: BigQuery,
   event: ExtendedEvent,
 ): Promise<void> => {
-  const eventInfo = parseSwapEvent(chainId, event.amm, event);
+  const eventInfo = parseSwapEvent(event);
 
   const swapRow = await pullExistingSwapRow(bigQuery, eventInfo.eventId);
 
