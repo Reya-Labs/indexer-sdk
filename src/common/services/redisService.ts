f250b925f88b22fb35aa022a3d896d575b307695
Artur Begyan <arturbegyan98@gmail.com>
2023-04-13 18:54:05 +0100

    feat: set from block redis

--- src/common/services/redisService.ts
+++ src/common/services/redisService.ts
@@ -7,15 +7,6 @@ redis.on('error', err => console.error('ERR:REDIS:', err));
 redis.on('connect',() => {console.log('connected to redis successfully!');});
 
 
-export const redisGet = async (
-    key: string
-): Promise<string | null> => {
-    const result = redis.get(key);
-
-    return result;
-
-}
-
 
 export const getFromBlock = async (
     tableId: string,
@@ -25,7 +16,20 @@ export const getFromBlock = async (
     
     const redisKey = `${tableId}_${chainId}_${vammAddress}`;
 
-    const fromBlock = Number(await redisGet(redisKey));
+    const fromBlock = Number(await redis.get(redisKey));
     
     return fromBlock;
+}
+
+export const setFromBlock = async (
+    tableId: string,
+    chainId: number,
+    vammAddress: string,
+    value: number
+): Promise<void> => {
+    
+    const redisKey = `${tableId}_${chainId}_${vammAddress}`;
+
+    await redis.set(redisKey, value);
+
 }
