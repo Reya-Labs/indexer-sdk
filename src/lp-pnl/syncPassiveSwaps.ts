9b8d895af89aaa8d2412624649d860d05550dcba
Artur Begyan <arturbegyan98@gmail.com>
2023-04-13 20:41:10 +0100

    feat: get passive swaps

--- src/lp-pnl/syncPassiveSwaps.ts
+++ src/lp-pnl/syncPassiveSwaps.ts
@@ -6,46 +6,12 @@ import { processPassiveSwapEvents } from './processPassiveSwapEvents';
 import { LP_PROCESSING_WINDOW, sleep } from '../common';
 
 export const syncPassiveSwaps = async (
+  tableId: string,
   bigQuery: BigQuery,
   amms: AMM[]
 ): Promise<void> => {
 
-    // // get min processing interval
-    // const minBlockInterval = LP_PROCESSING_WINDOW[chainId];
-
-    // while (true) {
-    //   const currentBlockNumber = await provider.getBlockNumber();
-  
-    //   if (previousBlockNumber === currentBlockNumber) {
-    //     console.log('Block has not changed. Sleeping...');
-    //     await sleep(60 * 1000); // sleep 60s
-    //     continue;
-    //   }
-  
-    //   console.log(`Processing blocks: ${previousBlockNumber}-${currentBlockNumber}`);
-  
-    //   try {
-    //     console.log('Processing mints...');
-    //     await syncMints(chainId, bigQuery, amms, previousBlockNumber, currentBlockNumber);
-  
-    //     console.log('Processing passive swaps...');
-    //     await syncPassiveSwaps(
-    //       chainId,
-    //       bigQuery,
-    //       amms,
-    //       previousBlockNumber,
-    //       currentBlockNumber,
-    //       minBlockInterval,
-    //     );
-  
-    //     previousBlockNumber = currentBlockNumber + 1;
-    //   } catch (error) {
-    //     console.log(`Loop has failed with message: ${(error as Error).message}.`);
-    //     await sleep(60 * 1000); // sleep 60s
-    //   }
-    // }
-
-  const previousSwapEvents = await getPreviousEvents(amms, ['swap']);
+  const previousSwapEvents = await getPreviousEvents(tableId, amms, ['swap']);
 
   const promises = Object.values(previousSwapEvents).map(async ({ events }) => {
 
