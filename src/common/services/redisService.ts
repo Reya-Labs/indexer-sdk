0317ac0223db57bec90924787c8f90c03ba366db
Artur Begyan <arturbegyan98@gmail.com>
2023-04-14 00:25:46 +0100

    feat: redis enhancement

--- src/common/services/redisService.ts
+++ src/common/services/redisService.ts
@@ -3,9 +3,12 @@ import { Redis } from 'ioredis';
 const REDISHOST = process.env.REDISHOST || 'localhost';
 const REDISPORT: number = Number(process.env.REDISPORT) || 6379;
 const redis = new Redis(REDISPORT, REDISHOST);
+let isRedisConnected = false;
+
 redis.on('error', (err) => console.error('ERR:REDIS:', err));
 redis.on('connect', () => {
   console.log('connected to redis successfully!');
+  isRedisConnected = true;
 });
 
 export const getFromBlock = async (
@@ -13,11 +16,20 @@ export const getFromBlock = async (
   chainId: number,
   vammAddress: string,
 ): Promise<number> => {
-  const redisKey = `${syncProcessName}_${chainId}_${vammAddress}`;
 
-  const fromBlock = Number(await redis.get(redisKey));
+  if (isRedisConnected) {
 
-  return fromBlock;
+    const redisKey = `${syncProcessName}_${chainId}_${vammAddress}`;
+
+    const fromBlock = Number(await redis.get(redisKey));
+  
+    return fromBlock;
+
+  } else {
+    console.log('redis is not connected, defaulting to block 0');
+    return 0; 
+  }
+
 };
 
 export const setFromBlock = async (
@@ -26,7 +38,11 @@ export const setFromBlock = async (
   vammAddress: string,
   value: number,
 ): Promise<void> => {
-  const redisKey = `${syncProcessName}_${chainId}_${vammAddress}`;
 
-  await redis.set(redisKey, value);
+  if (isRedisConnected) { 
+    const redisKey = `${syncProcessName}_${chainId}_${vammAddress}`;
+
+    await redis.set(redisKey, value);
+  } 
+
 };
