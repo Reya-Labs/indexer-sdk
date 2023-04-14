6f8e09056b38c564676e5b331e3afb5852206410
Artur Begyan <arturbegyan98@gmail.com>
2023-04-14 12:33:32 +0100

    feat: introduce cache set window to mints and burns sync

--- src/mints-and-burns/sync.ts
+++ src/mints-and-burns/sync.ts
@@ -8,25 +8,31 @@ import { processMintOrBurnEvent } from './processMintAndBurnEvent';
 export const sync = async (bigQuery: BigQuery, amms: AMM[], redisClient?: Redis): Promise<void> => {
   const previousMintEvents = await getPreviousEvents('mint_burn', amms, ['mint', 'burn'], bigQuery);
 
-  const promises = Object.values(previousMintEvents).map(async ({ events }) => {
+  const promises = Object.values(previousMintEvents).map(async ({ events, fromBlock }) => {
 
     // since all events that belong to a given vamm have the same chain id
     const cacheSetWindow = CACHE_SET_WINDOW[events[0].chainId];
+    let latestCachedBlock = fromBlock;
 
     for (const event of events) {
       await processMintOrBurnEvent(bigQuery, event);
 
-      await setFromBlock(
-        {
-          syncProcessName: 'mint_burn',
-          chainId: event.chainId,
-          vammAddress: event.address,
-          lastBlock: event.blockNumber,
-          redisClient: redisClient,
-          bigQuery: bigQuery
-        }
-      );
-      
+      const currentBlock = event.blockNumber;
+      const currentWindow = currentBlock - latestCachedBlock;
+      if (currentWindow > cacheSetWindow) {
+
+        await setFromBlock(
+          {
+            syncProcessName: 'mint_burn',
+            chainId: event.chainId,
+            vammAddress: event.address,
+            lastBlock: event.blockNumber,
+            redisClient: redisClient,
+            bigQuery: bigQuery
+          }
+        );
+
+      }      
     }
   });
 
