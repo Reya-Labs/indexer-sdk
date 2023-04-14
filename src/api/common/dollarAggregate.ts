18228319b78dff1b328c3371f51181c33606ead3
Artur Begyan <arturbegyan98@gmail.com>
2023-04-14 10:09:25 +0100

    feat: disable dollar agg eslint

--- src/api/common/dollarAggregate.ts
+++ src/api/common/dollarAggregate.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import axios from 'axios';
 
 export const getCurrentEthER = async (geckoKey: string): Promise<number> => {
