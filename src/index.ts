0824aaf22c02606c829ce5a4d542ca7c35902a15
Artur Begyan <arturbegyan98@gmail.com>
2023-04-14 01:08:10 +0100

    feat: conditional redis connection

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,21 @@ import { PROJECT_ID, REDISHOST, REDISPORT } from './common';
 import { run as runLPs } from './lp-pnl/run';
 import { run as runMintsAndBurns } from './mints-and-burns/run';
 import { run as runTraders } from './trader-pnl/run';
-
 import { Redis } from 'ioredis';
-const redis = new Redis(REDISPORT, REDISHOST);
-let redisConnected = false;
 
-redis.on('connect', () => {
-  console.log('successfully connected to redis');
-  redisConnected = true;
-}); 
+let redisConnected = false;
+let redisClient: Redis | undefined;
+
+if ((REDISHOST !== undefined) && (REDISPORT !== undefined)) { 
+
+  redisClient = new Redis(REDISPORT, REDISHOST);
+
+  redisClient.on('connect', () => {
+    console.log('successfully connected to redis');
+    redisConnected = true;
+  }); 
+
+}
 
 
 async function authenticateImplicitWithAdc() {
@@ -31,9 +37,9 @@ const main = async () => {
   let promises: Promise<void>[] = [];
 
   if (redisConnected) { 
-    promises = promises.concat(runMintsAndBurns(chainIds, redis));
-    promises = promises.concat(runTraders(chainIds, redis));
-    promises = promises.concat(runLPs(chainIds, redis));
+    promises = promises.concat(runMintsAndBurns(chainIds, redisClient));
+    promises = promises.concat(runTraders(chainIds, redisClient));
+    promises = promises.concat(runLPs(chainIds, redisClient));
   } else { 
     promises = promises.concat(runMintsAndBurns(chainIds));
     promises = promises.concat(runTraders(chainIds));
