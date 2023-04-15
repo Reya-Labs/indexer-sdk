dc2e12c2b09f5d622e19b9c4fea172515b586da4
Artur Begyan <arturbegyan98@gmail.com>
2023-04-15 23:09:35 +0100

    feat: lp pnl speed

---
+++ src/lp-pnl-speed/processPassiveSwapEvents/generatePassiveSwapEvent.ts
@@ -0,0 +1,63 @@
+import { getFixedTokenDeltaUnbalanced } from '../../common/services';
+import { SwapEventInfo } from '../../common/swaps/parseSwapEvent';
+
+export type GeneratePassiveSwapEventArgs = {
+  ownerAddress: string;
+  tickLower: number;
+  tickUpper: number;
+
+  variableTokenDelta: number;
+  fixedTokenDelta: number;
+
+  eventTimestamp: number;
+
+  startTimestamp: number;
+  maturityTimestamp: number;
+
+  variableFactorStartToCurrent: number;
+  rootEventInfo: SwapEventInfo;
+};
+
+export const generatePassiveSwapEvent = ({
+  variableTokenDelta,
+  fixedTokenDelta,
+  ownerAddress,
+  tickLower,
+  tickUpper,
+  eventTimestamp,
+  startTimestamp,
+  maturityTimestamp,
+  variableFactorStartToCurrent,
+  rootEventInfo,
+}: GeneratePassiveSwapEventArgs): SwapEventInfo => {
+  const fixedTokenDeltaUnbalanced = getFixedTokenDeltaUnbalanced({
+    variableTokenDelta,
+    fixedTokenDelta,
+    currentTimestamp: eventTimestamp,
+    startTimestamp,
+    maturityTimestamp,
+    variableFactorStartToCurrent,
+  });
+
+  // todo: come up with an id structure for passive swap: not high prio
+  const eventId = `id`;
+
+  const passiveSwapEvent: SwapEventInfo = {
+    eventId,
+    eventBlockNumber: rootEventInfo.eventBlockNumber,
+    chainId: rootEventInfo.chainId,
+    vammAddress: rootEventInfo.vammAddress,
+    ownerAddress,
+    tickLower,
+    tickUpper,
+    variableTokenDelta,
+    fixedTokenDeltaUnbalanced,
+    feePaidToLps: 0, // does not apply to passive swaps
+    rateOracle: rootEventInfo.rateOracle,
+    underlyingToken: rootEventInfo.underlyingToken,
+    marginEngineAddress: rootEventInfo.marginEngineAddress,
+    amm: rootEventInfo.amm,
+  };
+
+  return passiveSwapEvent;
+};
---
+++ src/lp-pnl-speed/syncPassiveSwaps.ts
@@ -0,0 +1,60 @@
+import { BigQuery } from '@google-cloud/bigquery';
+import { AMM } from '@voltz-protocol/v1-sdk';
+import { Redis } from 'ioredis';
+
+import { applyProcessingWindow, CACHE_SET_WINDOW, getPreviousEvents, setFromBlock } from '../common';
+import { LP_PROCESSING_WINDOW } from '../common';
+import { processPassiveSwapEvents } from './processPassiveSwapEvents';
+
+export const syncPassiveSwaps = async (
+  bigQuery: BigQuery,
+  amms: AMM[],
+  redisClient?: Redis,
+): Promise<void> => {
+  const previousSwapEvents = await getPreviousEvents('passive_swaps_lp', amms, ['swap'], bigQuery);
+
+  const promises = Object.values(previousSwapEvents).map(async ({ events, fromBlock }) => {
+    const chainId = events[0].chainId;
+    const minBlockInterval = LP_PROCESSING_WINDOW[chainId];
+    const eventsWithInterval = applyProcessingWindow(events, minBlockInterval);
+
+    const cacheSetWindow = CACHE_SET_WINDOW[events[0].chainId];
+    let latestCachedBlock = fromBlock;
+
+    for (const event of eventsWithInterval) {
+      await processPassiveSwapEvents({
+        bigQuery,
+        event,
+      });
+
+      const currentBlock = event.blockNumber;
+      const currentWindow = currentBlock - latestCachedBlock;
+
+      if (currentWindow > cacheSetWindow) {
+
+        const isSet = await setFromBlock(
+          {
+            syncProcessName: 'passive_swaps_lp',
+            chainId: event.chainId,
+            vammAddress: event.address,
+            lastBlock: event.blockNumber,
+            redisClient: redisClient,
+            bigQuery: bigQuery
+  
+          }
+        );
+
+        latestCachedBlock = isSet ? event.blockNumber : latestCachedBlock;
+
+      }
+    
+    }
+  });
+
+  const output = await Promise.allSettled(promises);
+  output.forEach((v) => {
+    if (v.status === 'rejected') {
+      throw v.reason;
+    }
+  });
+};
