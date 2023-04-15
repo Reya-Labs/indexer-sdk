d36b6211f26c259d19ad2543c0718c63df6b7a2c
Artur Begyan <arturbegyan98@gmail.com>
2023-04-15 23:35:23 +0100

    feat: switcher process lp speed event

--- src/lp-pnl-speed/processLpSpeedEvent/processLpSpeedEvent.ts
+++ src/lp-pnl-speed/processLpSpeedEvent/processLpSpeedEvent.ts
@@ -1,12 +1,25 @@
 import { BigQuery } from '@google-cloud/bigquery';
 import { ExtendedEvent } from '../../common/types';
+import { MintOrBurnEventInfo, SwapEventInfo, parseEvent } from '../../common/event-parsers';
 
 export const processLpSpeedEvent = async (
   bigQuery: BigQuery,
   event: ExtendedEvent,
 ): Promise<void> => {
 
-  const eventInfo = parseEvent(event);
+  const eventInfo: SwapEventInfo | MintOrBurnEventInfo = parseEvent(event);
+
+  switch(eventInfo.type) {
+
+    case ('mint' || 'burn'): { 
+      await processMintOrBurnEventLpSpeed(event); 
+    }
+
+    case 'swap': {
+      await processSwapEventLpSpeed(event); 
+    }
+
+  }
 
   
 };
