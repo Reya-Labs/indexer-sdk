3f411b50aba08d6862e32467df0d3a9aca9a6001
Artur Begyan <arturbegyan98@gmail.com>
2023-04-16 18:33:52 +0100

    feat: parse vamm price change event

--- src/common/event-parsers/parseEvent.ts
+++ src/common/event-parsers/parseEvent.ts
@@ -1,15 +1,18 @@
 import { ExtendedEvent } from '../types';
 import { MintOrBurnEventInfo, parseMintOrBurnEvent } from './parseMintOrBurnEvent';
-import { parseSwapEvent,SwapEventInfo } from './parseSwapEvent';
+import { parseSwapEvent, SwapEventInfo } from './parseSwapEvent';
+import { parseVAMMPriceChangeEvent, VAMMPriceChangeEventInfo } from './parseVAMMPriceChangeEvent';
 
-export const parseEvent = (event: ExtendedEvent): SwapEventInfo | MintOrBurnEventInfo => {
-  let parsedEvent: SwapEventInfo | MintOrBurnEventInfo;
+export const parseEvent = (event: ExtendedEvent): SwapEventInfo | MintOrBurnEventInfo | VAMMPriceChangeEventInfo => {
+  let parsedEvent: SwapEventInfo | MintOrBurnEventInfo | VAMMPriceChangeEventInfo;
 
   switch (event.type) {
     case 'mint' || 'burn': {
       parsedEvent = parseMintOrBurnEvent(event);
     }
-
+    case 'price_change': {
+      parsedEvent = parseVAMMPriceChangeEvent(event);
+    }
     default: {
       parsedEvent = parseSwapEvent(event);
     }
---
+++ src/common/event-parsers/parseVAMMPriceChangeEvent.ts
@@ -0,0 +1,48 @@
+import { AMM } from '@voltz-protocol/v1-sdk';
+import { ExtendedEvent } from '../types';
+
+export type VAMMPriceChangeEventInfo = {
+  eventId: string;
+  eventBlockNumber: number;
+  chainId: number;
+  vammAddress: string;
+  ownerAddress: string;
+  tickLower: number;
+  tickUpper: number;
+
+  tick: number;
+
+  rateOracle: string;
+  underlyingToken: string;
+  marginEngineAddress: string;
+  amm: AMM;
+  type: string;
+  eventTimestamp: number;
+};
+
+export const parseVAMMPriceChangeEvent = (event: ExtendedEvent): VAMMPriceChangeEventInfo => {
+  const eventId = `${event.blockHash}_${event.transactionHash}_${event.logIndex}`;
+  const ownerAddress = event.args?.recipient as string;
+  const tickLower = event.args?.tickLower as number;
+  const tickUpper = event.args?.tickUpper as number; 
+  const tick = event.args?.tick as number;
+
+  const amm = event.amm;
+
+  return {
+    eventId: eventId.toLowerCase(),
+    eventBlockNumber: event.blockNumber,
+    chainId: event.chainId,
+    vammAddress: event.amm.id.toLowerCase(),
+    ownerAddress: ownerAddress.toLowerCase(),
+    tickLower,
+    tickUpper,
+    tick,
+    rateOracle: event.amm.rateOracle.protocol,
+    underlyingToken: event.amm.underlyingToken.name,
+    marginEngineAddress: event.amm.marginEngineAddress,
+    amm,
+    type: event.type,
+    eventTimestamp: event.timestamp,
+  };
+};
