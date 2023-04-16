3b72534f263e9c715229890da0f68ba2803751f2
Artur Begyan <arturbegyan98@gmail.com>
2023-04-16 01:33:19 +0100

    feat: skeleton for process swap event lp speed

--- src/lp-pnl-speed/processLpSpeedEvent/processSwapEventLpSpeed.ts
+++ src/lp-pnl-speed/processLpSpeedEvent/processSwapEventLpSpeed.ts
@@ -1,11 +1,46 @@
 import { BigQuery } from "@google-cloud/bigquery";
 import { SwapEventInfo } from "../../common/event-parsers";
+import { generateLpPositionUpdatesQuery, pullExistingLpPositionRows } from "../../big-query-support";
+import { generateLpPositionRowsFromPassiveSwaps } from "../../lp-pnl/processPassiveSwapEvents/generateLpPositionRowsFromPassiveSwaps";
+
+export const processSwapEventLpSpeed = async (bigQuery: BigQuery, rootEventInfo: SwapEventInfo): Promise<void> => {
+
+  // Retrieve all LPs
+  const existingLpPositionRows = await pullExistingLpPositionRows(
+    bigQuery,
+    rootEventInfo.amm.id,
+    rootEventInfo.eventTimestamp,
+  );
 
 
+  const { passiveSwapEvents, affectedLps } = await gPassiveSwapEvents({
+    existingLpPositionRows,
+    amm: rootEventInfo.amm,
+    rootEventInfo
+  });
 
+  if (affectedLps.length === 0) {
+    // since the latest checkpoint, no lps were affected by passive swaps
+    return;
+  }
 
-export const processSwapEventLpSpeed = async (bigQuery: BigQuery, eventInfo: SwapEventInfo): Promise<void> => {
+  const lpPositionRows = await generateLpPositionRowsFromPassiveSwaps({
+    passiveSwapEvents,
+    affectedLps,
+    chainId: rootEventInfo.chainId,
+    amm: rootEventInfo.amm,
+    eventTimestamp: rootEventInfo.eventTimestamp,
+    eventBlockNumber: rootEventInfo.eventBlockNumber,
+  });
 
-    
+  const sqlTransactionQuery = generateLpPositionUpdatesQuery(lpPositionRows);
+
+  const options = {
+    query: sqlTransactionQuery,
+    timeoutMs: 100000,
+    useLegacySql: false,
+  };
+
+  await bigQuery.query(options);
 
 }
