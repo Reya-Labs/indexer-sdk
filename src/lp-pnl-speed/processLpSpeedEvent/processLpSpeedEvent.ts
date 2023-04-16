b4fd1180cffe1b2b6007949b41aafddafe4c73ef
Artur Begyan <arturbegyan98@gmail.com>
2023-04-16 18:37:47 +0100

    feat: skeleton for lp speed event

--- src/lp-pnl-speed/processLpSpeedEvent/processLpSpeedEvent.ts
+++ src/lp-pnl-speed/processLpSpeedEvent/processLpSpeedEvent.ts
@@ -1,19 +1,20 @@
 import { BigQuery } from '@google-cloud/bigquery';
 
-import { MintOrBurnEventInfo, parseEvent,SwapEventInfo } from '../../common/event-parsers';
+import { MintOrBurnEventInfo, parseEvent, SwapEventInfo, VAMMPriceChangeEventInfo } from '../../common/event-parsers';
 import { ExtendedEvent } from '../../common/types';
 import { processMintOrBurnEventLpSpeed } from './processMintOrBurnEventLpSpeed';
-import { processVAMMPriceChangeEvent } from './processVAMMPriceChangeEvent';
 
 export const processLpSpeedEvent = async (
   bigQuery: BigQuery,
   event: ExtendedEvent,
 ): Promise<void> => {
-  const eventInfo: VAMMPriceChangeEventInfo | MintOrBurnEventInfo = parseEvent(event);
+  const eventInfo: VAMMPriceChangeEventInfo | MintOrBurnEventInfo | SwapEventInfo = parseEvent(event);
 
-  if ('variableTokenDelta' in eventInfo) {
+  if ('tick' in eventInfo) {
     await processVAMMPriceChangeEvent(bigQuery, eventInfo);
-  } else {
+  } else if ('notionalDelta' in eventInfo) {
     await processMintOrBurnEventLpSpeed(bigQuery, eventInfo);
+  } else {
+    throw Error("Swap events are not necessary when processing lp speed events");
   }
 };
