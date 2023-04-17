f3ca564725f3831531d8e728afa72801ab1623df
Artur Begyan <arturbegyan98@gmail.com>
2023-04-17 19:09:48 +0100

    feat: define positions table schema

--- src/big-query-support/manage-tables/positions-table.ts
+++ src/big-query-support/manage-tables/positions-table.ts
@@ -5,9 +5,19 @@ export const getPositionsTable = async (tableName: string, bigQuery: BigQuery): 
 
     // todo: check typings and add tests 
     
-    const dataset = bigQuery.dataset(DATASET_ID);
+    const [tables] = await bigQuery.dataset(DATASET_ID).getTables();
 
-    const [table] = await dataset.table(tableName).get();
+    console.log(tables.length);
+
+    const table: Table | undefined = tables.find(
+      (table) => { 
+        return table.id === tableName;
+      }
+    );
+
+    if (table === undefined) { 
+      return null;
+    }
 
     return table;
 
@@ -18,20 +28,41 @@ export const createPositionsTable = async (tableName: string, bigQuery: BigQuery
   const existingTable: Table | null = await getPositionsTable(tableName, bigQuery);
 
   if (existingTable !== null) {
+    console.log("A positions table already exists"); 
     return; 
   }
 
-  const schema = `
+  console.log("Creating a new positions table"); 
 
-    marginEngineAddress:string, vammAddress:string, ownerAddress:string, 
-    tickLower:integer, tickUpper:integer, realizedPnLFromSwaps:numeric, 
-    realizedPnLFromFeesPaid:numeric, netNotionalLocked:numeric,
-    netFixedRate:numeric, lastUpdatedTimestamp:timestamp, notionalLiquidityProvided:numeric,
-    realizedPnLFromFeesCollected:numeric, netMarginDeposited:numeric, rateOracleIndex:numeric,
-    rowLastUpdatedTimestamp:timestamp, fixedTokenBalance:numeric, variableTokenBalance:numeric,
-    positionInitializationTimestamp:timestamp, rateOracle: string, underlyingToken: string,
-    chainId:integer, cashflowFactor:numeric(18,9), cashflowTimeFactor:numeric(18,9), cashflowFreeTerm:numeric(18,9),
-    liquidity:numeric(18,9), tickPrevious:integer`;
+  // const schema = 'marginEngineAddress:string, vammAddress:string, ownerAddress:string, tickLower:integer, tickUpper:integer, realizedPnLFromSwaps:numeric, realizedPnLFromFeesPaid:numeric, netNotionalLocked:numeric, netFixedRate:numeric, lastUpdatedTimestamp:timestamp, notionalLiquidityProvided:numeric, realizedPnLFromFeesCollected:numeric, netMarginDeposited:numeric, rateOracleIndex:numeric, rowLastUpdatedTimestamp:timestamp, fixedTokenBalance:numeric, variableTokenBalance:numeric, positionInitializationTimestamp:timestamp, rateOracle: string, underlyingToken: string, chainId:integer, cashflowFactor:numeric, cashflowTimeFactor:numeric, cashflowFreeTerm:numeric, liquidity:numeric, tickPrevious:integer';
+  const schema  = [
+    {name: 'marginEngineAddress', type: 'STRING', mode: 'REQUIRED'},
+    {name: 'vammAddress', type: 'STRING', mode: 'REQUIRED'},
+    {name: 'ownerAddress', type: 'STRING', mode: 'REQUIRED'},
+    {name: 'tickLower', type: 'INTEGER', mode: 'REQUIRED'},
+    {name: 'tickUpper', type: 'INTEGER', mode: 'REQUIRED'},
+    {name: 'realizedPnLFromSwaps', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'realizedPnLFromFeesPaid', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'netNotionalLocked', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'netFixedRateLocked', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'lastUpdatedTimestamp', type: 'TIMESTAMP', mode: 'REQUIRED'},
+    {name: 'notionalLiquidityProvided', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'realizedPnLFromFeesCollected', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'netMarginDeposited', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'rateOracleIndex', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'rowLastUpdatedTimestamp', type: 'TIMESTAMP', mode: 'REQUIRED'},
+    {name: 'fixedTokenBalance', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'variableTokenBalance', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'positionInitializationTimestamp', type: 'TIMESTAMP', mode: 'REQUIRED'},
+    {name: 'rateOracle', type: 'STRING', mode: 'REQUIRED'},
+    {name: 'underlyingToken', type: 'STRING', mode: 'REQUIRED'},
+    {name: 'chainId', type: 'INTEGER', mode: 'REQUIRED'},
+    {name: 'cashflowFactor', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'cashflowTimeFactor', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'cashflowFreeTerm', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'liquidity', type: 'NUMERIC', mode: 'REQUIRED', precision: '18', scale: '9'},
+    {name: 'tickPrevious', type: 'INTEGER', mode: 'REQUIRED'},
+  ]
 
   // For all options, see https://cloud.google.com/bigquery/docs/reference/v2/tables#resource
   const options = {
