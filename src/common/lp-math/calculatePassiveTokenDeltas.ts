0a18ee7a6dbbaa0238f09aeeab6a21e3e4c97ecc
Artur Begyan <arturbegyan98@gmail.com>
2023-04-17 16:36:27 +0100

    feat: refactor passive swap token delta calculation

--- src/common/lp-math/calculatePassiveTokenDeltas.ts
+++ src/common/lp-math/calculatePassiveTokenDeltas.ts
@@ -32,8 +32,25 @@ const  getAmount1Delta = (sqrtRatioAX96: JSBI, sqrtRatioBX96: JSBI, liquidity: J
       : JSBI.divide(JSBI.multiply(liquidity, JSBI.subtract(sqrtRatioBX96, sqrtRatioAX96)), Q96)
 }
 
+      
+/*
+    current price > previous price =>
+    current fixed rate < previous fixed rate =>
+    latest swap by the taker was a fixed taker swap
+    since the lp is taking the opposite side, they are
+    passively a variable taker, otherwise fixed taker
+*/
+export const iaVariableTakerSwap = (
+    tickCurrent: number, 
+    tickPrevious: number
+) => {
+    if (tickCurrent > tickPrevious) {
+        return true
+    }
+    return false;
+}
 
-
+// todo: this function can be simplified
 export const calculatePassiveTokenDeltas = (
     liquidity: number,
     tickUpper: number,
@@ -44,83 +61,59 @@ export const calculatePassiveTokenDeltas = (
     
     let variableTokenDelta: JSBI = ZERO;
     let fixedTokenDeltaUnbalanced: JSBI = ZERO;
-
-    // assume that this lp is affected, i.e. the check of wether it is affected or not is done before
-    // passing to this function
+    const liquidityJSBI: JSBI = JSBI.BigInt(liquidity);
+    let sqrtRatioA96: JSBI;
+    let sqrtRatioB96: JSBI;
+    let isVT: boolean = iaVariableTakerSwap(tickCurrent, tickPrevious);
 
     if (tickPrevious < tickLower) {
-
-        /*
-            if we assume that the lp was affected by this trade, it must be that the 
-            current tick is one of the two: 
-                (>= tickLower) && (< tickUpper)
-                > tickUpper
-        */
-
         if (tickCurrent < tickLower) {
-
             return { 
                 variableTokenDelta: ZERO,
                 fixedTokenDeltaUnbalanced: ZERO
             }
-
         }
-
-        const liquidityJSBI: JSBI = JSBI.BigInt(liquidity);
-        let sqrtRatioA96: JSBI;
-        let sqrtRatioB96: JSBI;
-
-        if ((tickCurrent >= tickLower) && (tickCurrent < tickUpper)) {
-
-            // lowerBound = tickLower
-            // upperBound = tickCurrent
-
-            // todo: write tests to check for precision
-            
+        if ((tickCurrent >= tickLower) && (tickCurrent < tickUpper)) {            
             sqrtRatioA96 = TickMath.getSqrtRatioAtTick(tickLower); 
             sqrtRatioB96 = TickMath.getSqrtRatioAtTick(tickCurrent);
-            
         } else {
             // i.e. > tickUpper
-
-
-            // lower bound = tickLower
-            // upper bound = tickUpper
-
             sqrtRatioA96 = TickMath.getSqrtRatioAtTick(tickLower); 
             sqrtRatioB96 = TickMath.getSqrtRatioAtTick(tickUpper);
-
+        }
+    } else if (tickPrevious < tickUpper) { 
+        if (tickCurrent < tickLower) {
+            sqrtRatioA96 = TickMath.getSqrtRatioAtTick(tickLower); 
+            sqrtRatioB96 = TickMath.getSqrtRatioAtTick(tickCurrent);
+        } else if ((tickCurrent >= tickLower) && (tickCurrent < tickUpper)) { 
+            sqrtRatioA96 = TickMath.getSqrtRatioAtTick(tickPrevious); 
+            sqrtRatioB96 = TickMath.getSqrtRatioAtTick(tickCurrent);
         }
 
-        /*  
-            in both of the two scenarios above: 
-            the latest swap has pushed the price up, hence pushed the fixed rate down
-            since it pushed the fixed rate down, it must have been a fixed taker swap from trader's (taker's) perspective
-            which in turn means it is a variable taker swap from lp's (maker's) perspective, hence the sign of
-            variable token delta is positive and sign of fixed token delta is negative
-        */ 
-
-        // todo: check if round up needs to be true or false when running sims and tests
-        variableTokenDelta = getAmount1Delta(
-            sqrtRatioA96,
-            sqrtRatioB96, 
-            liquidityJSBI,
-            true
-        );
         
-        // todo: check if round up needs to be true or false when running sims and tests
-        fixedTokenDeltaUnbalanced = getAmount0Delta(
-            sqrtRatioA96,
-            sqrtRatioB96, 
-            JSBI.multiply(NEGATIVE_ONE, liquidityJSBI),
-            true
-        );
 
-    } else if (tickPrevious < tickUpper) { 
+        
 
     } else {
 
     }
+
+
+    // todo: check if round up needs to be true or false when running sims and tests
+    variableTokenDelta = getAmount1Delta(
+        sqrtRatioA96,
+        sqrtRatioB96, 
+        isVT ? liquidityJSBI : JSBI.multiply(NEGATIVE_ONE, liquidityJSBI),
+        true
+    );
+
+    // todo: check if round up needs to be true or false when running sims and tests
+    fixedTokenDeltaUnbalanced = getAmount0Delta(
+        sqrtRatioA96,
+        sqrtRatioB96, 
+        isVT ? JSBI.multiply(NEGATIVE_ONE, liquidityJSBI) : liquidityJSBI,
+        true
+    );
 
     return { 
         variableTokenDelta: variableTokenDelta,
