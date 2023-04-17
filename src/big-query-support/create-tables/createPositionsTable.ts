2232b1f0368d634d20e023693ac69d68a58be08d
Artur Begyan <arturbegyan98@gmail.com>
2023-04-17 18:23:54 +0100

    feat: create position table script

--- src/big-query-support/create-tables/createPositionsTable.ts
+++ src/big-query-support/create-tables/createPositionsTable.ts
@@ -2,14 +2,21 @@ import { BigQuery } from "@google-cloud/bigquery";
 import { DATASET_ID, PROJECT_ID } from "../../common";
 
 
+export const createPositionTable = async (tableId: number, bigQuery: BigQuery) => {
 
+  const tableName = `${PROJECT_ID}.${DATASET_ID}.Voltz V1 Positions Staging ${tableId}`;  
 
-export const createPositionTable = async (tableId: number, bigQuery: BigQuery,) => {
+  const schema = `
 
-    const tableName = `${PROJECT_ID}.${DATASET_ID}.Voltz V1 Positions Staging ${tableId}`;  
-
-
-  const schema = 'Name:string, Age:integer, Weight:float, IsMagic:boolean';
+    marginEngineAddress:string, vammAddress:string, ownerAddress:string, 
+    tickLower:integer, tickUpper:integer, realizedPnLFromSwaps:numeric, 
+    realizedPnLFromFeesPaid:numeric, netNotionalLocked:numeric,
+    netFixedRate:numeric, lastUpdatedTimestamp:timestamp, notionalLiquidityProvided:numeric,
+    realizedPnLFromFeesCollected:numeric, netMarginDeposited:numeric, rateOracleIndex:numeric,
+    rowLastUpdatedTimestamp:timestamp, fixedTokenBalance:numeric, variableTokenBalance:numeric,
+    positionInitializationTimestamp:timestamp, rateOracle: string, underlyingToken: string,
+    chainId:integer, cashflowFactor:numeric(18,9), cashflowTimeFactor:numeric(18,9), cashflowFreeTerm:numeric(18,9),
+    liquidity:numeric(18,9), tickPrevious:integer`;
 
   // For all options, see https://cloud.google.com/bigquery/docs/reference/v2/tables#resource
   const options = {
