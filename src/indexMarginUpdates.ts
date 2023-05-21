01a93648d4035515c9f25dd52669ac95be26b8c5
Ioana Roceanu <ioanaroceanu@gmail.com>
2023-05-21 22:46:19 +0100

    feat: margin updates sync

---
+++ src/indexMarginUpdates.ts
@@ -0,0 +1,29 @@
+import { createMarginUpdatesTable } from './big-query-support/margin-updates-table/createActiveSwapsTable';
+import { sleep } from './common/utils';
+import { authenticateImplicitWithAdc, chainIds, indexInactiveTimeInMS } from './global';
+import { syncMarginUpdates } from './margin-updates/syncMarginUpdate';
+
+export const main = async () => {
+  await authenticateImplicitWithAdc();
+  await createMarginUpdatesTable();
+
+  while (true) {
+    try {
+      await syncMarginUpdates(chainIds);
+    } catch (error) {
+      console.log(
+        `[Margin Updates]: Loop has failed with message: ${(error as Error).message}.  It will retry...`,
+      );
+    }
+
+    await sleep(indexInactiveTimeInMS);
+  }
+};
+
+main()
+  .then(() => {
+    console.log('[Margin Updates]: Execution completed.');
+  })
+  .catch((error) => {
+    console.log(`[Margin Updates]: Error encountered. ${(error as unknown as Error).message}`);
+  });
